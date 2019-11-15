a4e8e3ff3ccbb620bb77952d697bbcbceb0a95d2
Shamil <shamilkhastiev2000@gmail.com>
2019-11-15 13:25:03 +0300

    not only Employee

--- database.sql
+++ database.sql
@@ -1,17 +1,47 @@
 CREATE TABLE Employee (
 
-	Fullname:          VARCHAR(30),
-	Address:           VARCHAR(30),
-	Dob:               VARCHAR(15),
-	Phone:             VARCHAR(12),
-	Email:             VARCHAR(30),
-	Qualification:     VARCHAR(20),
-	Contract:          VARCHAR(15),
-	Type:              VARCHAR(15),
-
-
-  
+	Fullname:      VARCHAR(30),
+	Address:       VARCHAR(30),
+	Dob:           VARCHAR(15),
+	Phone:         VARCHAR(12),
+	Email:         VARCHAR(30),
+	Qualification: VARCHAR(20),
+	Contract:      VARCHAR(15),
+	Type:          VARCHAR(15),
+	E_id           VARCHAR(15),
+	PRIMARY KEY (E_id)
 );
+
+CREATE TABLE Noticeboard(
+	News:	          VARCHAR(1000),
+	Upcoming_events:  VARCHAR(1000),
+	Date              DATE,
+	PRIMARY KEY(Date),
+	FOREIGN KEY (E_id) REFERENCES Employee(E_id)
+);
+
+CREATE TABLE Stuff_schedule(
+	Shedule:  VARCHAR(1000),
+	Date      DATE,
+	PRIMARY KEY(Date),
+	FOREIGN KEY (E_id) REFERENCES Employee(E_id)
+);
+
+CREATE TABLE Hospital_equipment(
+	HE_id	VARCHAR(15),
+	PRIMARY KEY (HE_id)
+);
+
+CREATE TABLE Medical_history(
+	Ilness 			 VARCHAR(30),
+	Duration		 VARCHAR(50),
+	Attending_doctor VARCHAR(30),
+	P_id 			 VARCHAR(15),
+	PRIMARY KEY (HE_id)
+);
+
+
+
 -- here we go
 -- CREATE TABLE Patient{
 -- 	P_ID : INT
