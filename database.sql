b4cdb26aa8a59e725a7970e1f58b2b9ed6fd95b9
Shamil <shamilkhastiev2000@gmail.com>
2019-11-15 11:24:35 +0300

    Employee

--- database.sql
+++ database.sql
@@ -1,6 +1,15 @@
-CREATE TABLE Doctor (
+CREATE TABLE Employee (
 
-	name: VARCHAR(15),
+	Fullname:          VARCHAR(30),
+	Address:           VARCHAR(30),
+	Dob:               VARCHAR(15),
+	Phone:             VARCHAR(12),
+	Email:             VARCHAR(30),
+	Qualification:     VARCHAR(20),
+	Contract:          VARCHAR(15),
+	Type:              VARCHAR(15),
+
+
   
 );
 -- here we go
