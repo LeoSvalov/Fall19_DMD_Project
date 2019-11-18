4a8b123239eba95fd1e932c9e4e26814647c1e92
Beka-13 <bekzhantalgat01@gmail.com>
2019-11-18 14:09:40 +0300

    Shamil lox

--- database.sql
+++ database.sql
@@ -12,67 +12,61 @@ DROP TABLE Get;
 DROP TABLE Make_an_appointment;
 DROP TABLE Employee;
 
-
 CREATE TABLE Employee (
-
-	Fullname      VARCHAR(30)   NOT NULL,   
+	Name      	  VARCHAR(30)   NOT NULL,
+	Surname       VARCHAR(30)   NOT NULL,   
 	Address       VARCHAR(30)   NOT NULL,
-	DoB           VARCHAR(15)   NOT NULL,
+	Dob           VARCHAR(15)   NOT NULL,
 	Phone         VARCHAR(12)   NOT NULL,
 	Email         VARCHAR(30)   NOT NULL,
 	Qualification VARCHAR(20)   NOT NULL,
 	Contract      VARCHAR(15)   NOT NULL,
 	Type          VARCHAR(15)   NOT NULL,
-	E_id           VARCHAR(15)   NOT NULL,
-	HE_id		   VARCHAR(15),
-	PRIMARY KEY (E_id)
+	E_ID          VARCHAR(15)   NOT NULL	PRIMARY KEY
 );
 
-CREATE TABLE Notice_board(
+CREATE TABLE Noticeboard(
 	News	          VARCHAR(1000),
 	Upcoming_events   VARCHAR(1000),
 	Date              DATE          NOT NULL,
-	E_id 	          VARCHAR(15)   REFERENCES Employee(E_id),
-	PRIMARY KEY(Date,E_id)
+	E_ID 	          VARCHAR(15)   REFERENCES Employee(E_ID),
+	PRIMARY KEY(Date,E_ID)
 );
 
 CREATE TABLE Stuff_schedule(
-	Schedule  VARCHAR(1000)  NOT NULL,
-	Date      DATE          NOT NULL,
-	E_id 	  VARCHAR(15)   REFERENCES Employee(E_id),
-	PRIMARY KEY(Date,E_id)
+	Shedule   VARCHAR(1000)  NOT NULL,
+	Date      DATE           NOT NULL,
+	E_ID 	  VARCHAR(15)    REFERENCES Employee(E_ID),
+	PRIMARY KEY(Date,E_ID)
 );
 
 CREATE TABLE Hospital_equipment(
-	HE_id VARCHAR(15)   NOT NULL,
-	PRIMARY KEY (HE_id)
+	HE_ID	VARCHAR(15)   NOT NULL	PRIMARY KEY
 );
 
 CREATE TABLE Medical_history(
-	Illness 			 VARCHAR(30)   NOT NULL,
+	M_ID			 VARCHAR(30)   NOT NULL		PRIMARY KEY(M_ID),
+	Ilness 			 VARCHAR(30)   NOT NULL,
 	Duration		 VARCHAR(50)   NOT NULL,
 	Attending_doctor VARCHAR(30)   NOT NULL,
-	P_id 			 VARCHAR(15)   NOT NULL,
-	PRIMARY KEY (P_id)
+	P_ID 			 VARCHAR(15)   NOT NULL		REFERENCES Patient(P_ID),
 );
 
 CREATE TABLE Optional_treatment(
 	Duration VARCHAR(30)   NOT NULL,
 	Price 	 INT           NOT NULL,
-	I_id	 VARCHAR(15)   NOT NULL,
-	PRIMARY KEY(I_id)
+	T_id	 VARCHAR(15)   NOT NULL		PRIMARY KEY(T_id)
 );
 
 CREATE TABLE Guest(
 	Name VARCHAR(30)   NOT NULL,
-	G_id VARCHAR(15)   NOT NULL,
-	PRIMARY KEY(G_id)
+	G_ID VARCHAR(15)   NOT NULL		PRIMARY KEY(G_ID)
 );
 
 CREATE TABLE Canteen_menu(
 	Type      VARCHAR(15)   NOT NULL,
-	E_id 	  VARCHAR(15)REFERENCES Employee(E_id),
-	PRIMARY KEY(Type,E_id)
+	E_ID 	  VARCHAR(15)	REFERENCES Employee(E_ID),
+	PRIMARY KEY(Type,E_ID)
 );
 
 CREATE TABLE Patient(
@@ -80,38 +74,78 @@ CREATE TABLE Patient(
 	Address       VARCHAR(30)   NOT NULL,
 	Dob           VARCHAR(15)   NOT NULL,
 	Ward_type     VARCHAR(12)   NOT NULL,
-	Room_num 	   VARCHAR(20)   NOT NULL,
-	Sex      	   VARCHAR(15)   NOT NULL,
+	Room_num 	  VARCHAR(20)   NOT NULL,
+	Sex      	  VARCHAR(15)   NOT NULL,
 	Type          VARCHAR(15)   NOT NULL,
-	P_id           VARCHAR(15)   NOT NULL,
-	PRIMARY KEY (P_id)
+	P_ID          VARCHAR(15)   NOT NULL	PRIMARY KEY
 );
 
 CREATE TABLE Donate(
-	P_ID VARCHAR(15)    NOT NULL,
-	E_ID VARCHAR(15)    NOT NULL,
+	P_ID VARCHAR(15)    NOT NULL 	REFERENCES Patient(P_ID),
+	E_ID VARCHAR(15)    NOT NULL	REFERENCES Employee(E_ID),
 	Amount_of_money INT NOT NULL,
 	PRIMARY KEY(P_ID,E_ID)
 );
 
 CREATE TABLE Visit(
-	P_ID VARCHAR(15)    NOT NULL,
-	E_ID VARCHAR(15)    NOT NULL,
+	P_ID VARCHAR(15)    NOT NULL 	REFERENCES Patient(P_ID),
+	E_ID VARCHAR(15)    NOT NULL	REFERENCES Employee(E_ID),
 	Time timestamp 		NOT NULL, 
 	PRIMARY KEY(P_ID,E_ID,Time)
 );
 
 CREATE TABLE Get(
-	P_ID VARCHAR(15)    NOT NULL,
-	T_ID VARCHAR(15)    NOT NULL,
+	P_ID VARCHAR(15)    NOT NULL 	REFERENCES Patient(P_ID),
+	T_ID VARCHAR(15)    NOT NULL	REFERENCES Optional_treatment(T_ID),
 	PRIMARY KEY(P_ID,T_ID)
 );
+
 CREATE TABLE Make_an_appointment(
-	P_ID VARCHAR(15)    NOT NULL,
-	E_ID VARCHAR(15)    NOT NULL,
+	P_ID VARCHAR(15)    NOT NULL 	REFERENCES Patient(P_ID),
+	E_ID VARCHAR(15)    NOT NULL	REFERENCES Employee(E_ID),
+	Date DATE 		    NOT NULL,
 	PRIMARY KEY(P_ID,E_ID)
 );
-	INSERT INTO Hospital_equipment VALUES
-	('qwe'),
-	('rewt'),
-	('feww');
+
+CREATE TABLE Negotiate_a_purchase(
+	Supply_manager		VARCHAR(30)		REFERENCES Employee(E_ID),
+	Economic_manager	VARCHAR(30)		REFERENCES Employee(E_ID),
+	HE_ID		  		VARCHAR(15)		REFERENCES Hospital_equipment(HE_ID),
+	NP_ID				VARCHAR(30)		PRIMARY KEY
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
